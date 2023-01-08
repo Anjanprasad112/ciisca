0ad16c1196a0a5f49b5b12aba6e24591b7631f44
Sid Karthik <sa268592@gmail.com>
2023-01-08 23:48:13 +0530

    added favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,29 @@ export default function Home() {
     <>
       <Head>
         <title>CIISCA</title>
-        <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/vnd.microsoft.icon"
+          href="/assets/favicon.ico"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon-16x16.png"
+        />
       </Head>
       <div className="w-full">
         <Header />
